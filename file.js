18c716730a09af776cd09954f25aea72a13dca62
sanaullahb <sanaullahb594@gmail.com>
2025-06-21 05:43:58 -0700

    added a basic javascript fie

---
+++ file.js
@@ -0,0 +1,14 @@
+// A simple JavaScript program
+
+// Greet the user
+console.log("Welcome to my simple JavaScript program!");
+
+// Ask the user for their name
+let name = prompt("What is your name?");
+
+// Display a personalized message
+if (name) {
+    console.log("Hello, " + name + "! Nice to meet you.");
+} else {
+    console.log("You didn't enter your name, but that's okay!");
+}
